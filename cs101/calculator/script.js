0073a171b6bde8e9db659935bb0761475a140988
Kalem <kalemgsmith@gmail.com>
2024-03-06 12:50:03 -0700

    better calc visuals

---
+++ cs101/calculator/script.js
@@ -0,0 +1,14 @@
+let buff = [];
+let op = "";
+
+function addNumberToBuff() {
+    alert("Adding number")
+}
+
+function setOperator() {
+    alert("Adding operator")
+}
+
+function equate() {
+    "Equating"
+}
