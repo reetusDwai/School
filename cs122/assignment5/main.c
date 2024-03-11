bd8f93fa2c057afe450ae1fba25f621c47c12d44
Kalem <kalemgsmith@gmail.com>
2024-03-11 12:02:00 -0600

    Getting this stuff working

---
+++ cs122/assignment5/main.c
@@ -0,0 +1,8 @@
+#include "queue.h"
+#include <stdio.h>
+
+int main(void) {
+  // Make a queue
+  Queue q1 = initQueue();
+  printf("this works");
+}
--- cs122/assignment5/queue.h
+++ cs122/assignment5/queue.h
@@ -1,14 +1,17 @@
-		
-		#include <stdlib.h>
- 
-        typedef struct {
-           int head, tail;
-           datatype QA[MaxQ];
-        } QType, *Queue;
- 
-        Queue initQueue();
-		int empty(Queue Q);
-        void enqueue(Queue Q, datatype n);
-        datatype dequeue(Queue Q);
-		
-		
+#include <stdio.h>
+#include <stdlib.h>
+#define MaxQ 12
+
+typedef struct {
+  int a;
+} datatype;
+
+typedef struct {
+  int head, tail;
+  datatype QA[MaxQ];
+} QType, *Queue;
+
+Queue initQueue();
+int empty(Queue Q);
+void enqueue(Queue Q, datatype n);
+datatype dequeue(Queue Q);
