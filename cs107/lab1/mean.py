6d394087be3ef2ce4c5c587cf3017fb38a23d136
Kalem <kalemgsmith@gmail.com>
2024-08-30 14:12:35 -0600

    part 2 done

--- cs107/lab1/mean.py
+++ cs107/lab1/mean.py
@@ -1,5 +1,21 @@
 # Complete the comments below
-# Name:
+# Name: Kalem Smith
 # Assignment: Lab 1, Exercise 2: mean.py
-# Date:
+# Date: 8/30/24
 # List any issues you had with this lab
+
+# Write a program that accepts 10 floating point numbers from the user and prints the running
+# average after each number is entered.
+
+sum = 0
+count = 0
+running_avg = 0
+
+for i in range(10):
+    num = int(input("Enter number: "))
+    sum += num
+    count += 1
+    running_avg = sum / count
+    print("Running Average: ", running_avg)
+
+
