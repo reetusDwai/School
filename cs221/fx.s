04d2a0e50c5ff6517d8df8f2bbf4921b04faa8c4
Kalem <kalemgsmith@gmail.com>
2024-09-09 12:38:29 -0600

    Finished till I learn more about printf

--- cs221/fx.s
+++ cs221/fx.s
@@ -8,6 +8,7 @@
 .section .note.GNU-stack,"",@progbits
 .section .data
     .comm buffer, 4 # Allocate 4 byte for the result of the function
+    format: .string "x = %= %04hx\n\0"
 .section .text
     .global _start
 # This program does not use stdlib. The linker recognizes _start
@@ -23,7 +24,7 @@ _start:
     # Convert buffer into a number
     movzbw buffer, %ax # Loads the first digit(10s place) into AX 
     # movzbw means move a byte into a word with higher order bits in the word being zero
-    sub $'0', %ax # Converts the ascii to an int by subtracting the ascii character 0
+    subw $'0', %ax # Converts the ascii to an int by subtracting the ascii character 0
     movw $10, %bx # Puts 10 into BX for multiplying
     # Multiplies AX by BX and stores it in AX
     mulw %bx
@@ -31,7 +32,7 @@ _start:
     # Convert second byte to a number
     # Loads the second digit into BX
     movzbw buffer+1, %bx
-    sub $'0', %bx
+    subw $'0', %bx
     addw %bx, %ax # Adds the second digit to the first digit giving us the correct digit
     movw %ax, buffer # Moves number back to buffer
 
@@ -49,6 +50,7 @@ _start:
     movq $buffer, %rsi # pointer to the buffer
     movq $4, %rdx # number of bytes to write
     syscall
+
     # Exit the program
     movq $60, %rax # syscall number for sys_exit (60)
     movq $0, %rdi # status = 0
