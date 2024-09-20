24269ad279d092167330c3c4484f160c737dadf9
Kalem <kalemgsmith@gmail.com>
2024-09-20 14:37:17 -0600

    Added comments

---
+++ cs221/lab2/collatz1.s
@@ -0,0 +1,10 @@
+# Write a program that prompts the user for a starting value. This starting
+# value must be greater than zero. In this event, the prompt will be repeated until a valid number is
+# entered. Once a valid starting value has been entered, print out “Proccesing...”, then the number, and
+# finally end your program. An interaction will look like this:
+# Enter a starting value greater than zero.
+# Start: 0
+# Start: 17
+# Processing...
+# 17
+
